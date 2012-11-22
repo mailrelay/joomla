3e28e9d223c167e318a0a6fd1833aeb26c68268a
yrobla <info@ysoft.biz>
2012-11-22 13:00:56 +0100

    redistributing files

--- source/plg_user_mailrelay/mailrelay.php
+++ source/plg_user_mailrelay/mailrelay.php
@@ -101,7 +101,7 @@ class plgUserMailrelay extends JPlugin
 			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 
                         $headers = array(
-                                'X-Request-Origin' => 'Joomla2.5|1.1|'.JPlatform::getShortVersion()
+                                'X-Request-Origin: Joomla2.5|1.1|'.JPlatform::getShortVersion()
                         );
                         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
